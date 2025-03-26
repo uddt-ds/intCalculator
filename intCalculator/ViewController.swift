c4e2599e6cf0f450078b9ed564fe7db549605c93
uddt <uddt_ds@kakao.com>
2025-03-26 20:33:20 +0900

    feat(Button): 버튼의 UI 추가
    - 첫번째 줄에 들어갈 버튼을 4개 추가하였습니다.

--- intCalculator/ViewController.swift
+++ intCalculator/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     private var number = 12345
 
+    var arrViews: [UIView] = []
+
     let label = UILabel()
+    let button7 = UIButton()
+    let button8 = UIButton()
+    let button9 = UIButton()
+    let plusButton = UIButton()
+    let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +33,58 @@ class ViewController: UIViewController {
         label.textColor = .white
         label.font = UIFont.boldSystemFont(ofSize: 60)
 
-        view.addSubview(label)
+        button7.setTitle("7", for: .normal)
+        button7.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        button7.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        button7.layer.cornerRadius = 40
 
-        label.translatesAutoresizingMaskIntoConstraints = false
+        button8.setTitle("8", for: .normal)
+        button8.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        button8.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        button8.layer.cornerRadius = 40
+
+        button9.setTitle("9", for: .normal)
+        button9.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        button9.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        button9.layer.cornerRadius = 40
+
+        plusButton.setTitle("+", for: .normal)
+        plusButton.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        plusButton.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+        plusButton.layer.cornerRadius = 40
+
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .black
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+
+        stackView.addArrangedSubview(button7)
+        stackView.addArrangedSubview(button8)
+        stackView.addArrangedSubview(button9)
+        stackView.addArrangedSubview(plusButton)
+
+        [label, stackView].forEach { view.addSubview($0) }
+
+        [label, stackView].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
 
         NSLayoutConstraint.activate([
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            label.heightAnchor.constraint(equalToConstant: 100)
+            label.heightAnchor.constraint(equalToConstant: 100),
+
+            button7.heightAnchor.constraint(equalToConstant: 80),
+            button7.widthAnchor.constraint(equalToConstant: 80),
+
+            button8.heightAnchor.constraint(equalToConstant: 80),
+            button8.widthAnchor.constraint(equalToConstant: 80),
+
+            button9.heightAnchor.constraint(equalToConstant: 80),
+            button9.widthAnchor.constraint(equalToConstant: 80),
+
+            stackView.heightAnchor.constraint(equalToConstant: 80),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 400),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
