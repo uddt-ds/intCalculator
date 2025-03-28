7490e086eef866e2f2b5f80788f8f86d78852724
uddt <uddt_ds@kakao.com>
2025-03-28 14:30:05 +0900

    refactor(stackView, UIButton): 나열하여 생성한 버튼을 묶어서 구현하였습니다.
    - 기존 UIButton
      UIButton을 나열하여 생성하고, 해당 Button을 스택뷰에 넣어주었습니다.
    
    - 변경 후 UIButton
      setButton 메서드를 통하여 title의 변화에 따라 버튼에 반영이 가능하도록 수정하였습니다.
      for문으로 버튼을 생성하고, 버튼의 설정을 적용하였습니다.

--- intCalculator/ViewController.swift
+++ intCalculator/ViewController.swift
@@ -11,70 +11,21 @@ class ViewController: UIViewController {
 
     private var number = 12345
 
-    let label = UILabel()
-    var stackView1 = UIStackView()
-    var stackView2 = UIStackView()
+    var arrButtons: [UIButton] = []
 
-    let arr = [
-        ["7", "8", "9", "+"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "*"],
-        ["AC", "0", "=", "/"]
-    ]
+    var titles = ["7", "8", "9", "+"]
+
+    let label = UILabel()
+    let button = UIButton()
+    let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        configureStackView()
-
-    }
-    func configureStackView() {
-        self.stackView1 = makeHorizontalStackView(arr[0])
-        self.stackView2 = makeHorizontalStackView(arr[1])
-        
-        for i in arr {
-            stackView1 =  makeHorizontalStackView(i)
-            break
-        }
-        view.addSubview(stackView1)
-        NSLayoutConstraint.activate([
-            stackView1.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 60),
-            stackView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25)
-        ])
-    }
-
-    func makeHorizontalStackView(_ buttonTitles: [String]) -> UIStackView {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.backgroundColor = .black
-        stackView.spacing = 10
-        stackView.distribution = .fillEqually
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            stackView.heightAnchor.constraint(equalToConstant: 80),
-        ])
-
-        for buttonTitle in buttonTitles {
-            let button = UIButton()
-            button.setTitle(buttonTitle, for: .normal)
-            button.titleLabel?.font = .boldSystemFont(ofSize: 30)
-            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
-            button.layer.cornerRadius = 40
-
-            button.translatesAutoresizingMaskIntoConstraints = false
-            NSLayoutConstraint.activate([
-                button.heightAnchor.constraint(equalToConstant: 80),
-                button.widthAnchor.constraint(equalToConstant: 80)
-            ])
-
-            stackView.addArrangedSubview(button)
-        }
-
-        return stackView
     }
 
     private func configureUI() {
+        arrButtons = setButton(titles, button)
         view.backgroundColor = .black
 
         label.text = "\(number)"
@@ -82,24 +33,43 @@ class ViewController: UIViewController {
         label.textColor = .white
         label.font = UIFont.boldSystemFont(ofSize: 60)
 
-        view.addSubview(label)
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .black
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
 
-        label.translatesAutoresizingMaskIntoConstraints = false
+        // arrViews에서 담겨진 [UIButton]을 하나씩 순차적으로 담아주면 되지 않을까?
+        stackView.addArrangedSubview(arrButtons[0])
+        stackView.addArrangedSubview(arrButtons[1])
+        stackView.addArrangedSubview(arrButtons[2])
+        stackView.addArrangedSubview(arrButtons[3])
+
+        [label, stackView].forEach { view.addSubview($0) }
+
+        [label, stackView].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
 
         NSLayoutConstraint.activate([
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            label.heightAnchor.constraint(equalToConstant: 100)
+            label.heightAnchor.constraint(equalToConstant: 100),
+
+            stackView.heightAnchor.constraint(equalToConstant: 80),
+            stackView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 60),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
+
+    // Title자리만 바꿔주고 싶다.
+    func setButton(_ titles : [String], _ button: UIButton) -> [UIButton] {
+        for title in titles {
+            let button = UIButton()
+            button.setTitle("\(title)", for: .normal)
+            arrButtons.append(button)
+            button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+            button.layer.cornerRadius = 40
+        }
+        return arrButtons
+    }
 }
-
-// 이근나 T 피드백
-// 4개의 열을 만들기
-// 수평으로 쌓이는 스택뷰 - 4개
-// 버튼 1개를 재사용이 가능한 상태로 바꾸기
-// 16개의 버튼을 만들기
-
-// 버튼을 만들어서 스택뷰에 넣는거 자체를 하나로 만들거나 << 얘로 방향을 잡고 시작.
-// 버튼 만들기 / 스택뷰 만들기
-// 버튼에 태그 (있는지 확인만)
